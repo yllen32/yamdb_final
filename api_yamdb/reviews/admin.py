edba2b006f6fb3d8dc499ce8393af616bc12f6ce
yllen32 <furtov1995@yandex.ru>
2022-12-06 04:03:32 +0300

    add comma

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -6,7 +6,7 @@ from .models import User, Categorie, Genre, Title, Comment, Review
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'role', 'email',)
-    list_editable = ('role', 'email')
+    list_editable = ('role', 'email',)
     exclude = ('groups', 'user_permissions', 'is_staff')
     search_fields = ('username', 'email')
     readonly_fields = ('last_login', 'date_joined', 'password',)
